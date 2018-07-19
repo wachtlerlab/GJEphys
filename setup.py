16a4f2f0543a9dd429103a476a5f0bce8e7fe838
Ajayrama Kumaraswamy <ajkumaraswamy@tutamail.com>
2018-07-19 16:40:37 +0200

    added dependency traceback2, froze neo version requirement

--- setup.py
+++ setup.py
@@ -10,13 +10,15 @@ setup(
                       "scipy>=0.18.1",
                       "pandas>=0.19.0",
                       "seaborn>=0.7.1",
-                      "neo>=0.5",
+                      # 0.6 had problems with importing Spike2 Files
+                      "neo==0.5",
                       "nixio>=0.1.3",
                       "pylatex",
                       "xlrd>=1.0.0",
                       "openpyxl>=2.4.5",
                       "scikit-learn>=0.18.1",
-                      "requests>=2.14.2"],
+                      "requests>=2.14.2",
+                      "traceback2>=1.4"],
 
     python_requires=">=2.7"
 
